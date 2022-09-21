396384a1c53476d5782b305351d773540fece48b
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-21 07:19:51 +0100

    Add the side to side nav

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
-const libraryBooksEl = document.querySelector('.library-books');
+const listEl = document.querySelector('.header-list');
+const addnewEl = document.querySelector('.header-addnew');
+const contactEl = document.querySelector('.header-contact');
+const date = document.querySelector('.date');
+const libraryBooksListEl = document.querySelector('.library-booklist');
+const libraryBooksEl = document.querySelector('.library-booklist__books')
 const addElBtn = document.querySelector('.library-btn__add');
 const removeElBtn = document.getElementsByClassName('library-btn__rmv');
+const contactInformationEl = document.querySelector('.contact-information');
+const form = document.querySelector('form');
 const titleEl = document.getElementById('title');
 const authorEl = document.getElementById('author');
 const errMsgEl = document.querySelector('.errMsg');
+setInterval(() => {
+  date.innerHTML = new Date()
+}, 1000);
+
+
+const toggleWindow = () => {
+    listEl.addEventListener('click', () => {
+      form.classList.remove('show');
+      contactInformationEl.classList.remove('show');
+      libraryBooksListEl.classList.remove('hide');
+      listEl.style.color = 'blue';
+      addnewEl.style.color = 'black';
+      contactEl.style.color = 'black';
+    })
+
+        
+    addnewEl.addEventListener('click', () => {
+      form.classList.add('show');
+      contactInformationEl.classList.remove('show');
+      libraryBooksListEl.classList.add('hide');
+      listEl.style.color = 'black';
+      addnewEl.style.color = 'blue';
+      contactEl.style.color = 'black';
+
+  }) 
+
+    contactEl.addEventListener('click', () => {
+        form.classList.remove('show');
+        contactInformationEl.classList.add('show');
+        libraryBooksListEl.classList.add('hide');
+        listEl.style.color = 'black';
+        addnewEl.style.color = 'black';
+        contactEl.style.color = 'blue';
+    })
+
+}
 
 const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
@@ -16,7 +59,7 @@ const renderBooks = () => {
       <p class="library-book__title">"${elem.title}"</p> <span> by </span>
       <p class="library-book__author">${elem.author}</p>
       
-      <a href="index.html"><button type="button" class="library-btn__rmv border-black" id=${index}>Remove</button></a>
+      <a href=""><button type="button" class="library-btn__rmv border-black" id=${index}>Remove</button></a>
   </div>`;
   });
   libraryBooksEl.innerHTML = markup;
@@ -31,7 +74,7 @@ class Library {
 
   addBook() {
     localStorage.setItem('bookList', JSON.stringify(bookList));
-    addElBtn.addEventListener('click', () => {
+    addElBtn.addEventListener('click', (e) => {
       this.title = titleEl.value;
       this.author = authorEl.value;
       if (this.title && this.author) {
@@ -44,7 +87,12 @@ class Library {
         renderBooks();
         errMsgEl.innerHTML = '';
       } else {
+        e.preventDefault();
         errMsgEl.innerHTML = 'Input something';
+        setTimeout(() => {
+          errMsgEl.innerHTML = '';
+        }, 2000);
+        
       }
     });
   }
@@ -67,3 +115,4 @@ const awsomeBooks = new Library();
 renderBooks();
 awsomeBooks.addBook();
 awsomeBooks.removeBook();
+toggleWindow();
