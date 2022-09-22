b904a7687da00e50885727ecde2900de2b51cff4
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-22 21:30:16 +0100

    Clean code successfully

--- app.js
+++ app.js
@@ -5,10 +5,44 @@ const date = document.querySelector('.date');
 const libraryBooksListEl = document.querySelector('.library-booklist');
 const libraryBooksEl = document.querySelector('.library-booklist__books');
 const addElBtn = document.querySelector('.library-btn__add');
+const contactInformationEl = document.querySelector('.contact-information');
+const form = document.querySelector('form');
 const titleEl = document.getElementById('title');
 const authorEl = document.getElementById('author');
 const errMsgEl = document.querySelector('.errMsg');
-const form = document.querySelector('form');
+setInterval(() => {
+  date.innerHTML = new Date();
+}, 1000);
+
+const toggleWindow = () => {
+  listEl.addEventListener('click', () => {
+    form.classList.remove('show');
+    contactInformationEl.classList.remove('show');
+    libraryBooksListEl.classList.remove('hide');
+    listEl.style.color = 'blue';
+    addnewEl.style.color = 'black';
+    contactEl.style.color = 'black';
+  });
+
+  addnewEl.addEventListener('click', () => {
+    form.classList.add('show');
+    contactInformationEl.classList.remove('show');
+    libraryBooksListEl.classList.add('hide');
+    listEl.style.color = 'black';
+    addnewEl.style.color = 'blue';
+    contactEl.style.color = 'black';
+  });
+
+  contactEl.addEventListener('click', () => {
+    form.classList.remove('show');
+    contactInformationEl.classList.add('show');
+    libraryBooksListEl.classList.add('hide');
+    listEl.style.color = 'black';
+    addnewEl.style.color = 'black';
+    contactEl.style.color = 'blue';
+  });
+};
+
 const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
 class Library {
@@ -34,12 +68,12 @@ class Library {
         errMsgEl.style.color = 'blue';
       } else {
         errMsgEl.innerHTML = 'Input something';
+        errMsgEl.style.color = 'red';
       }
       setTimeout(() => {
         errMsgEl.innerHTML = '';
       }, 800);
     });
-    libraryBooksEl.innerHTML = markup;
   }
 
   renderBooks() {
@@ -70,8 +104,8 @@ class Library {
     removeBook();
   }
 }
-
 const awesomeBooks = new Library();
 
 awesomeBooks.addBook();
 awesomeBooks.renderBooks();
+toggleWindow();
