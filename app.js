f0777c9a8327378d145cc3f6151d66978d57f722
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-22 20:49:02 +0100

    Cleaning code better

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
 const libraryBooksEl = document.querySelector('.library-books');
 const addElBtn = document.querySelector('.library-btn__add');
-const removeElBtn = document.getElementsByClassName('library-btn__rmv');
 const titleEl = document.getElementById('title');
 const authorEl = document.getElementById('author');
 const errMsgEl = document.querySelector('.errMsg');
-
+const form = document.querySelector('form');
 const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
-localStorage.setItem('bookList', JSON.stringify(bookList));
-
-const renderBooks = () => {
-  let markup = '';
-  JSON.parse(localStorage.getItem('bookList')).forEach((elem, index) => {
-    markup += `<div class="library-book" style="background-color: ${index % 2 && 'rgb(225, 223, 223)'}">
-      <p class="library-book__title">"${elem.title}"</p> <span> by </span>
-      <p class="library-book__author">${elem.author}</p>
-      
-      <a href="index.html"><button type="button" class="library-btn__rmv border-black" id=${index}>Remove</button></a>
-  </div>`;
-  });
-  libraryBooksEl.innerHTML = markup;
-};
-
 class Library {
-  constructor(title, author, id) {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.id = id;
   }
 
   addBook() {
-    localStorage.setItem('bookList', JSON.stringify(bookList));
     addElBtn.addEventListener('click', () => {
-      this.title = titleEl.value;
-      this.author = authorEl.value;
-      if (this.title && this.author) {
+      const title = titleEl.value;
+      const author = authorEl.value;
+      if (title && author) {
         const newBook = {
-          title: this.title,
-          author: this.author,
+          title,
+          author,
         };
         bookList.push(newBook);
         localStorage.setItem('bookList', JSON.stringify(bookList));
-        renderBooks();
+        this.renderBooks();
+        form.reset();
         errMsgEl.innerHTML = '';
       } else {
         errMsgEl.innerHTML = 'Input something';
@@ -49,21 +32,36 @@ class Library {
     });
   }
 
-  removeBook() {
-    const { id } = this;
-    for (let i = 0; i < removeElBtn.length; i += 1) {
-      removeElBtn[i].addEventListener('click', (e) => {
-        bookList.splice(e.target.id, 1);
-        localStorage.setItem('bookList', JSON.stringify(bookList));
-        renderBooks();
-        return id;
+  renderBooks() {
+    if (!bookList.length) {
+      libraryBooksEl.innerHTML = 'No books added';
+    } else {
+      let markup = '';
+      bookList.forEach((elem, index) => {
+        markup += `<div class="library-book" style="background-color: ${index % 2 && 'rgb(225, 223, 223)'}">
+        <p class="library-book__title">"${elem.title}"</p> <span> by </span>
+        <p class="library-book__author">${elem.author}</p>    
+        <button type="button" class="library-btn__rmv" id=${index}>Remove</button>
+    </div>`;
       });
+      libraryBooksEl.innerHTML = markup;
     }
+
+    const removeBook = () => {
+      const removeBtnsEl = [...document.getElementsByClassName('library-btn__rmv')];
+      removeBtnsEl.forEach((item) => {
+        item.addEventListener('click', (e) => {
+          bookList.splice(e.target.id, 1);
+          localStorage.setItem('bookList', JSON.stringify(bookList));
+          this.renderBooks();
+        });
+      });
+    };
+    removeBook();
   }
 }
 
-const awsomeBooks = new Library();
+const awesomeBooks = new Library();
 
-renderBooks();
-awsomeBooks.addBook();
-awsomeBooks.removeBook();
+awesomeBooks.addBook();
+awesomeBooks.renderBooks();
